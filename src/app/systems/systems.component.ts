d30774a0897538b05ce408d4df27414a78c1c535
Erik Svensson <arizon.es@gmail.com>
2023-09-01 20:23:36 +0200

    iterate over systems

--- src/app/systems/systems.component.ts
+++ src/app/systems/systems.component.ts
@@ -11,33 +11,11 @@ import { Subscription } from 'rxjs';
   templateUrl: './systems.component.html',
   styleUrls: ['./systems.component.css']
 })
-export class SystemsComponent implements OnInit, OnDestroy {
+export class SystemsComponent implements OnInit {
+  systemResps: SystemStatusResponse[] | undefined;
 
-  systemResps!: SystemStatusResponse[];
-  displayCert = false;
-  alertUrlSub!: Subscription | undefined;
-  systemFg = this.fb.group({
-    id: [{value: '', disabled: true}], 
-    name: ['', Validators.required],
-    alertBody: ['', Validators.required, Validators.maxLength(500)],
-    alertEmail: [''],
-    alertUrl: [''],
-    callBody: [''],
-    callUrl: ['', Validators.required],
-    certExpirationDays: [''],
-    certStatus: [{value: '', disabled: true}],
-    clientCertId: [], 
-    httpMethod: [''],
-    lastFailTime: [{value: '', disabled: true}],
-    lastOkTime: [{value: '', disabled: true}],
-    message: [''],
-    responseMatch: ['', Validators.required],
-    status: [{value: '', disabled: false}]
-  })
-
-  constructor(private systemSvc: SystemService, private errHandler: ErrorHandlerService, private fb: FormBuilder) { }
-
-  ngOnInit(): void {
+  constructor (private systemSvc: SystemService, private errHandler: ErrorHandlerService) {}
+  ngOnInit() {
     const statusRespSub = this.systemSvc.getSystems().subscribe({
       next: (data: SystemStatusResponse[]) => {
         if (data) {
@@ -51,18 +29,5 @@ export class SystemsComponent implements OnInit, OnDestroy {
         statusRespSub.unsubscribe();
       }
     });
-    this.alertUrlSub = this.systemFg.controls['alertUrl'].valueChanges.subscribe(value => {
-      if (value?.includes("https")) {
-        this.displayCert = true;
-      } else {
-        this.displayCert = false;
-      }
-    })
-  }
-  ngOnDestroy(): void {
-      if (this.alertUrlSub) {
-        this.alertUrlSub.unsubscribe();
-      }
-  }
-
+  } 
 }
