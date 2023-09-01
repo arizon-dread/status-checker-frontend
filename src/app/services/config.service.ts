ad9c073dad3fbed8ac1610aa3aface03572d8d84
Arizon <arizon.es@gmail.com>
2023-09-01 20:34:37 +0200

    Remove commented code

--- src/app/services/config.service.ts
+++ src/app/services/config.service.ts
@@ -13,39 +13,11 @@ export class ConfigService {
   public static config: Config = {apiBaseUrl: ""};
 
   loadConfig(): Observable<Config> {
-    //const promise = 
     console.log("loading config..");
-    // const confSub = this.http.get<Config>("/config/config.json").subscribe({
-    //   next: (data: Config) => {
-    //     this.config = data;
-    //     return of(data).toPromise();      
-    //   },
-    //   error: (err: Error) => {
-    //     this.errHandler.handleError("loadConfig", this.config, "Unable to load config", true);
-        
-    //   },
-    //   complete: () => {
-    //     confSub.unsubscribe();
-    //   }
-      
-    // });
-    //return of(this.config).toPromise();
-    // return new Promise((resolve, reject) => {
-    //   const config = this.http.get<Config>("/config/config.json").toPromise();
-    // });
     
-    return this.http.get<Config>("/config/config.json")
-    //.toPromise<Config>()
-        // .then(data => {
-        //   ConfigService.config = data as Config;
-        //   console.log(ConfigService.config);
-        //   //Object.assign(this, data)
-          
-        //   return data;//Promise.resolve(data);
-        // });
+    
+    return this.http.get<Config>("/config/config.json");
 
-
-    //return promise;
   }
   
 }
