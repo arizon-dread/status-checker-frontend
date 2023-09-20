61c64858a565c5c03e7abd0cdca68ef7913fa43f
Erik Svensson <arizon.es@gmail.com>
2023-09-20 18:43:25 +0200

    add more functionality

--- src/app/systems/services/system.service.ts
+++ src/app/systems/services/system.service.ts
@@ -14,11 +14,18 @@ export class SystemService {
   constructor(private http: HttpClient, private errHandler: ErrorHandlerService) { }
 
   systems: SystemStatusResponse[] | undefined;
+  system: SystemStatusResponse | undefined;
   getSystems() : Observable<SystemStatusResponse[]> {
     console.log("config: "+ ConfigService.config.apiBaseUrl);
     return this.http.get<SystemStatusResponse[]>(ConfigService.config.apiBaseUrl + '/systemstatus')
       .pipe(
         catchError(this.errHandler.handleError("getSystems", this.systems, "Unable to get systems from backend", true))
+      );
+  }
+  getSystem(id: number) : Observable<SystemStatusResponse> {
+    return this.http.get<SystemStatusResponse>(ConfigService.config.apiBaseUrl + '/systemstatus/' + id)
+      .pipe(
+        catchError(this.errHandler.handleError("getSystem", this.system, "Unable to get system from backend"))
       );
   }
 }
--- src/app/systems/system-item/system-item.component.ts
+++ src/app/systems/system-item/system-item.component.ts
@@ -1,28 +1,44 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { SystemStatusResponse } from '../models/system-status-response';
 import { faCircleCheck, faCircleExclamation, faCircleXmark } from '@fortawesome/free-solid-svg-icons';
+import { SystemService } from '../services/system.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-system-item',
   templateUrl: './system-item.component.html',
   styleUrls: ['./system-item.component.css']
 })
-export class SystemItemComponent implements OnInit {
+export class SystemItemComponent implements OnInit, OnDestroy {
 
   @Input() system: SystemStatusResponse | undefined;
   faCircleCheck = faCircleCheck;
-  class = 'text-success'
-  constructor() { }
+  class = 'text-success';
+  title = 'System is up';
+  systemSub: Subscription | undefined;
+  constructor(private systemSvc: SystemService) { }
 
   ngOnInit(): void {
     if (this.system?.status === '') {
       this.class = 'text-warning';
       this.faCircleCheck = faCircleExclamation;
+      this.title = 'It seems this system has never been polled.'
     } else if (this.system?.status !== 'OK') {
       this.class = 'text-danger';
       this.faCircleCheck = faCircleXmark;
+      this.title = 'The last poll was deemed unsuccessful'
     }
-
+  }
+  ngOnDestroy(): void {
+      if (this.systemSub) {
+        this.systemSub.unsubscribe();
+      }
+  }
+  poll() {
+    if (this.system) {
+      this.systemSvc.getSystem(this.system?.id);
+    }
+    
   }
 
 }
