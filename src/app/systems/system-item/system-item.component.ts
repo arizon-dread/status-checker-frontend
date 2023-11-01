20f28d06d5fed2f56de37d019a44fce0a4aeb1a5
Erik Svensson <arizon.es@gmail.com>
2023-11-01 23:12:39 +0100

    fix style + subscribe to call

--- src/app/systems/system-item/system-item.component.ts
+++ src/app/systems/system-item/system-item.component.ts
@@ -1,8 +1,9 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, DestroyRef, Input, OnDestroy, OnInit, inject } from '@angular/core';
 import { SystemStatusResponse } from '../models/system-status-response';
 import { faCircleCheck, faCircleExclamation, faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 import { SystemService } from '../services/system.service';
 import { Subscription } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-system-item',
@@ -15,6 +16,9 @@ export class SystemItemComponent implements OnInit, OnDestroy {
   faCircleCheck = faCircleCheck;
   class = 'text-success';
   title = 'System is up';
+  dateOrNeverCalled = "Never polled";
+  dateStyle = 'fst-italic';
+  destroyRef = inject(DestroyRef)
   systemSub: Subscription | undefined;
   constructor(private systemSvc: SystemService) { }
 
@@ -28,6 +32,10 @@ export class SystemItemComponent implements OnInit, OnDestroy {
       this.faCircleCheck = faCircleXmark;
       this.title = 'The last poll was deemed unsuccessful'
     }
+    if (this.system?.lastOkTime.includes("0001-01-01")) {
+      this.dateOrNeverCalled = this.system?.lastOkTime ?? "";
+      this.dateStyle = "";
+    } 
   }
   ngOnDestroy(): void {
       if (this.systemSub) {
@@ -36,7 +44,16 @@ export class SystemItemComponent implements OnInit, OnDestroy {
   }
   poll() {
     if (this.system) {
-      this.systemSvc.getSystem(this.system?.id);
+      this.systemSvc.getSystem(this.system?.id).pipe(takeUntilDestroyed(this.destroyRef)).subscribe({
+        next: (data: SystemStatusResponse) => {
+          if (data) {
+            this.system = data;
+          }
+        },
+        error: (err: Error) => {
+          console.log("Error while polling: " + err.message);
+        }      
+      });
     }
     
   }
