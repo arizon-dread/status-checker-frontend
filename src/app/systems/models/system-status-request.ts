cd1b374f7e926363163babdd38d5bf87ec5444f3
Erik Svensson <arizon.es@gmail.com>
2024-02-24 21:46:28 +0100

    add fields to request model

--- src/app/systems/models/system-status-request.ts
+++ src/app/systems/models/system-status-request.ts
@@ -1,10 +1,12 @@
 export interface SystemStatusRequest {
     responseMatch: string
     alertBody: string
-    alertEmail: string
-    alertUrl: string
+    alertEmail?: string
+    alertUrl?: string
     callUrl: string
+    callBody?: string
     httpMethod: string
+    clientCertId?: number
     name: string
-    id: number
+    id?: number
 }
