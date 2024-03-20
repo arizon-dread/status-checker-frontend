d20283e27da60594f0f4f24bcba941f362a28f37
Erik Svensson <arizon.es@gmail.com>
2024-03-20 22:36:42 +0100

    add trailing ;

--- src/app/systems/systems.component.ts
+++ src/app/systems/systems.component.ts
@@ -64,7 +64,7 @@ export class SystemsComponent implements OnInit {
             error: (err: HttpErrorResponse) => {
               console.log(err.message);
             }
-          })
+          });
         }
       }
     });
