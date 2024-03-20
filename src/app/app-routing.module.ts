ae0ed969f63eab8868c1c0cfef797346a8e1a908
Erik Svensson <arizon.es@gmail.com>
2024-03-20 22:36:56 +0100

    add welcome component and default route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SystemsComponent } from './systems/systems.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 import { SettingsComponent } from './settings/settings.component';
 
 const routes: Routes = [
   { path: 'settings', component: SettingsComponent },
-  { path: 'systems', loadChildren: () => import('./systems/systems.module').then(m => m.SystemsModule) }
+  { path: 'systems', loadChildren: () => import('./systems/systems.module').then(m => m.SystemsModule) },
+  { path: '**', component: WelcomeComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    WelcomeComponent,
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
