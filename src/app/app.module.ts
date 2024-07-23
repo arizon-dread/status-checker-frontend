dbc6cbacedac082f334d7ac9377f77fbd166a2ba
Erik Svensson <arizon.es@gmail.com>
2024-07-23 22:42:33 +0200

    update angular/cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ConfigService } from './services/config.service';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ToastrModule } from 'ngx-toastr';
 import { tap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
@@ -19,30 +19,23 @@ function appConfigInit(appConfigService: ConfigService): () => Observable<any> {
        tap(config => { ConfigService.config = config })
     );
  }
-@NgModule({
-  declarations: [
-    AppComponent,
-
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    SharedModule,
-    HttpClientModule,
-    ToastrModule.forRoot({
-      progressBar: true,
-      preventDuplicates: true
-    })
-  ],
-  providers: [
-    {
-      provide: APP_INITIALIZER,
-      useFactory: appConfigInit,
-      multi: true,
-      deps: [ConfigService, HttpClient]
-    }
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        SharedModule,
+        ToastrModule.forRoot({
+            progressBar: true,
+            preventDuplicates: true
+        })], providers: [
+        {
+            provide: APP_INITIALIZER,
+            useFactory: appConfigInit,
+            multi: true,
+            deps: [ConfigService, HttpClient]
+        },
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule { }
