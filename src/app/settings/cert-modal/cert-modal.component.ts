7af6e4ef935137f0e81b5000ab750a247e9c1bea
Erik Svensson <arizon.es@gmail.com>
2024-07-24 23:52:58 +0200

    empty component for updating/adding certs

---
+++ src/app/settings/cert-modal/cert-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cert-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './cert-modal.component.html',
+  styleUrl: './cert-modal.component.css'
+})
+export class CertModalComponent {
+
+}
